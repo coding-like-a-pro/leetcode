ea519630e201a1b0c5bbf7bc421b52af91cffa04
saloev <saloev.saadi@yandex.ru>
2020-11-29 22:10:09 +0300

    update: consecutive charachters

--- saloev/consecutive-characters.ts
+++ saloev/consecutive-characters.ts
@@ -1,21 +1,17 @@
 function maxPower(s: string): number {
   let currentCharCount = 0;
-  let previousCharCount = 0;
   let currentChar = s[0];
   let res = 1;
-
-  const lastMax = () => Math.max(previousCharCount, currentCharCount);
 
   for (const char of s) {
     if (char === currentChar) {
       currentCharCount += 1;
     } else {
       currentChar = char;
-      res = Math.max(res, lastMax());
-      previousCharCount = currentCharCount;
+      res = Math.max(res, currentCharCount);
       currentCharCount = 1;
     }
   }
 
-  return Math.max(res, lastMax());
+  return Math.max(res, currentCharCount);
 }
