010365e6ce42ac30db9459825245da5609436fad
saloev <saloev.saadi@yandex.ru>
2020-11-29 15:03:49 +0300

    contranct progmraning

--- design-hashmap.ts
+++ design-hashmap.ts
@@ -1,4 +1,10 @@
 class MyHashMap {
+  /**
+   * inv: size >= 0
+   * inv: capacity >= 0
+   * inv: map number[][]
+   * inv: capacity >= 0
+   * */
   private map: number[][];
   private inserted: boolean[];
   private size: number;
@@ -11,10 +17,20 @@ class MyHashMap {
     this.capacity = 10;
   }
 
+  /**
+   * 
+   * pre: 
+   * key - >= 0
+   */
   hash(key: number): number {
     return key % this.capacity;
   }
+  // post: number <= capacity
 
+  /**
+   * Думаю тут Pre не нужен из из инвариантов которые у нас есть
+   * 
+   */
   ensureCapacity() {
     const is75PercentFull = Math.round(this.size / this.capacity) * 100 >= 75;
     if (!is75PercentFull) return;
@@ -22,7 +38,18 @@ class MyHashMap {
     this.capacity = 2 * this.capacity;
     this.reHash();
   }
+  /**
+   * Post: 
+   * if size / capacity  <= 0.75 nothing
+   * else 
+   * capacity = 2 * this.capacity';
+   * [key, value] in map every must be in [key, value] in map'
+   * inserted = boolean[]
+   * */
 
+  /**
+   * 
+   */
   reHash() {
     const saveData = this.map;
     this.map = [];
@@ -36,13 +63,30 @@ class MyHashMap {
       this.put(key, value);
     }
   }
+  /** 
+   * [key, value] in map every must be in [key, value] in map'
+   * inserted = boolean[]
+   * */
 
+   /**
+    * Pre: key >= 0
+    */
   findNextPos(key: number) {
     return this.findPos(key, true);
   }
+  /**
+   * Post: 
+   * number >= 0
+   */
 
+  /**
+   * Pre:
+   * key >= 0
+   */
   findPos(key: number, isNeedLastPos: boolean) {
+    // inv: pos -> number
     let pos = this.hash(key);
+    // inv: this.inserted[pos] === true || this.inserted[pos] === undefined
     while (this.inserted[pos]) {
       if (this.map[pos][0] === key) return pos;
       pos = (pos + 1) % this.capacity;
@@ -50,22 +94,52 @@ class MyHashMap {
 
     return isNeedLastPos ? pos : -1;
   }
+  /**
+   * Post: 
+   * if (key in map) pos in this.map
+   * else if (isNeedLastPos) 0 <= pos <= capacity
+   * else -1
+   */
 
+  /**
+   * key >= 0
+   * 
+   */ 
   put(key: number, value: number): void {
     this.ensureCapacity();
 
+    // inv: 0<= index <= this.capacity 
     const index = this.findNextPos(key);
     this.map[index] = [key, value];
     this.inserted[index] = true;
     this.size += 1;
   }
+  /**
+   * Post: 
+   * [key, value] in map
+   * inserted[index] === true
+   * size = size' + 1
+   */
 
+   /**
+    * Pre:
+    * key >= 0
+    */
   get(key: number): number {
     const index = this.findPos(key, false);
     if (index === -1) return -1;
     return this.map[index][1];
   }
+  /**
+   * Post:
+   * -1 or value in map
+   */
 
+
+   /**
+    * Pre:
+    * key >= 0
+    */
   remove(key: number): void {
     this.ensureCapacity();
 
@@ -75,4 +149,11 @@ class MyHashMap {
     this.map[index] = [];
     this.size -= 1;
   }
+  /**
+   * Post:
+   * if (key in map)
+   *  map[index]
+   *  size = size' - 1;
+   * else nothing
+   */
 }
